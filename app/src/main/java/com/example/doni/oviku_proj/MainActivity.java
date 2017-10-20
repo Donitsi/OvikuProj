efbf8f3f79b40d55d03e14246e79ec77f73531db
Doni Phan <t3phdo00@students.oamk.fi>
2017-10-20 12:37:00 +0300

    New Homescreen

--- app/src/main/java/com/example/doni/oviku_proj/MainActivity.java
+++ app/src/main/java/com/example/doni/oviku_proj/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.doni.oviku_proj;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 
 public class MainActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
