8c80f4aa15d7eeb886b3556ed47db6ecc6cbe098
Doni Phan <t3phdo00@students.oamk.fi>
2017-10-20 16:27:31 +0300

    Bottom toolbar added

--- app/src/main/java/com/example/doni/oviku_proj/MainPage.java
+++ app/src/main/java/com/example/doni/oviku_proj/MainPage.java
@@ -1,9 +1,13 @@
 package com.example.doni.oviku_proj;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainPage extends AppCompatActivity {
 
@@ -32,6 +36,32 @@ public class MainPage extends AppCompatActivity {
                 return true;
             }
         });
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+
+                switch (item.getItemId()){
+                    case R.id.action_keys:
+                        Toast.makeText(MainPage.this, "Action Keys clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_access:
+                        Toast.makeText(MainPage.this, "Action Access clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_history:
+                        Toast.makeText(MainPage.this, "Action History clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_more:
+                        Toast.makeText(MainPage.this, "Action More clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 
     public void OpenAndCloseLockDoor(){
