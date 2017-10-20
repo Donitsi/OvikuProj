73288f355e9037d81c898e03941421f72ecd06f5
Doni Phan <t3phdo00@students.oamk.fi>
2017-10-20 13:05:31 +0300

    Homepage skeleton somewhat done

--- app/src/main/java/com/example/doni/oviku_proj/HomePage.java
+++ app/src/main/java/com/example/doni/oviku_proj/HomePage.java
@@ -1,13 +1,15 @@
 package com.example.doni.oviku_proj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_page);
     }
 }
