321e7d9f8775cd10825b4d86a4b0e55c1adee83d
Doni Phan <t3phdo00@students.oamk.fi>
2017-10-27 12:34:20 +0300

    New launcher icon added

---
+++ app/src/main/java/com/example/doni/oviku_proj/HistoryPageFragment.java
@@ -0,0 +1,11 @@
+package com.example.doni.oviku_proj;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Doni on 10/26/2017.
+ */
+
+public class HistoryPageFragment extends Fragment {
+
+}
