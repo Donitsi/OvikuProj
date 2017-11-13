916186f5c4027cdeedba470c0ea2f579f22fa666
Doni Phan <t3phdo00@students.oamk.fi>
2017-11-13 12:06:58 +0200

    Added new launcher icon and logos

--- app/src/main/java/com/example/doni/oviku_proj/HomePageFragment.java
+++ app/src/main/java/com/example/doni/oviku_proj/HomePageFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Doni on 10/25/2017.
@@ -16,6 +17,8 @@ import android.widget.ImageView;
 public class HomePageFragment extends Fragment {
 
     private boolean firstImageShown = true;
+    TextView loadingText;
+
 
     @Nullable
     @Override
@@ -25,6 +28,10 @@ public class HomePageFragment extends Fragment {
 
         ImageView settingsButton = (ImageView) v.findViewById(R.id.settings_button);
         ImageView lockImage = (ImageView)v.findViewById(R.id.vLock);
+
+
+        loadingText = (TextView)v.findViewById(R.id.loadingText);
+        loadingText.setText("Loading");
 
         settingsButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -39,12 +46,12 @@ public class HomePageFragment extends Fragment {
             public void onClick(View v){
                 if(firstImageShown){
                     ImageView lockImage1 = (ImageView)v.findViewById(R.id.vLock);
-                    lockImage1.setImageResource(R.drawable.close_lock);
+                    lockImage1.setImageResource(R.drawable.close_lock1);
                     firstImageShown = false;
                 }
                 else{
                     ImageView lockImage1 = (ImageView)v.findViewById(R.id.vLock);
-                    lockImage1.setImageResource(R.drawable.open_lock);
+                    lockImage1.setImageResource(R.drawable.open_lock1);
                     firstImageShown = true;
                 }
             }
@@ -53,12 +60,13 @@ public class HomePageFragment extends Fragment {
         lockImage.setOnLongClickListener(new View.OnLongClickListener(){
             public boolean onLongClick(View v){
                 ImageView lockImage1 = (ImageView)v.findViewById(R.id.vLock);
-                lockImage1.setImageResource(R.drawable.secure_lock);
+                lockImage1.setImageResource(R.drawable.secure_lock1);
                 firstImageShown = true;
                 return true;
             }
         });
         return v;
     }
+
 
 }
